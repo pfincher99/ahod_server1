02191d676e8cdf0ca5afc990c6caf5aa995a9fff
Pete Fincher <pfincher@cisco.com>
2016-11-08 19:59:48 -0500

    testing branch to dev

--- demoapp.py
+++ demoapp.py
@@ -1,7 +1,6 @@
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 
-
 app = Flask(__name__)
 app = Flask(__name__)
 api = Api(app)
@@ -23,3 +22,5 @@ api.add_resource(Alert, '/alert')
 if __name__ == '__main__':
     # Run Flask
     app.run(debug=True, host='0.0.0.0', port=int("5000"))
+
+# testing text
