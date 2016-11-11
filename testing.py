7f2bc497b800c556efbbd6f7150288be6f3bd14f
Pete Fincher <pfincher@cisco.com>
2016-11-11 06:54:19 -0500

    edited os.environ entries

--- testing.py
+++ testing.py
@@ -2,9 +2,6 @@ import demoapp
 import unittest
 import os
 
-room_id = os.environ["SPARK_ROOM"]
-spark_token = "Bearer " + os.environ["SPARK_TOKEN"]
-
 class FlaskTestCase(unittest.TestCase):
 
     def setUp(self):
