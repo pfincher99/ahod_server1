1b5f20c57cc9bd8f2703b7446a52d6fda50c5a4b
Pete Fincher <pfincher@cisco.com>
2016-11-18 13:09:33 -0500

    added comments

--- demoapp.py
+++ demoapp.py
@@ -7,14 +7,16 @@ import requests
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 
-db_loc = "ahod.db"
 
-# Spark information
+# Information required for Spark
 room_url = 'https://api.ciscospark.com/v1/messages'
 room_id = os.getenv("spark_room")
 token = os.getenv("spark_token")
 spark_token = "Bearer "+str(token)
 
+
+#SQLite3 database for storing PLC data posted from the IE4000
+db_loc = "ahod.db"
 def create_db():
     global conn, cur
     # check to see if db exists, if not, create
@@ -38,18 +40,16 @@ def create_db():
         cur.close()
 
 
-## Flask Server
+## Flask Web Server
 app = Flask(__name__)
 api = Api(app)
 
 
-# Start: Hello world code not used for API only for testing at this stage
+# Start: This code not used for API only for testing at this stage
 class HelloWorld(Resource):
     def get(self):
         text = "Hello World! This is the Master branch"
         return text
-
-
 api.add_resource(HelloWorld, '/hello/world')
 
 
@@ -57,14 +57,11 @@ class Alert(Resource):
     def get(self):
         text = "Alert Received on Master Branch"
         return text
-
-
 api.add_resource(Alert, '/alert')
+# End: code not used for API only for testing at this stage
 
 
-# End: Hello world code not used for API only for testing at this stage
-
-# API code for ahod web server
+# Handler to respond to a GET request for the latest posted PLC data
 @app.route("/ahod", methods=['GET'])
 def validate():
     conn = sqlite3.connect(db_loc)
@@ -89,7 +86,7 @@ def validate():
                                     "plcIp": plcIp}})
     cur.close()
 
-
+# Handler to receive PLC data posted from the IE4000 application
 @app.route("/ahod", methods=['POST'])
 def receiver():
     global post_data
@@ -100,37 +97,7 @@ def receiver():
     post_spark()
     return jsonify(result={"status": 200})
 
-# Code to get the SIP URI of the Spark Room
-@app.route("/uri", methods=['GET'])
-def get_sip_uri():
-    global room_id, spark_token, sip_addr
-    get_room_url = 'https://api.ciscospark.com/v1/rooms/' + str(room_id)
-    headers = {'content-type':'application/json','Authorization': spark_token}
-    payload = {'showSipAddress': 'true'}
-    spark_post = requests.get(get_room_url, headers=headers, params=payload)
-    conv_response_json = spark_post.json()
-    sip_addr = conv_response_json[u'sipAddress']
-    return sip_addr
-
-
-def post_spark():
-    global room_url, switchName, message, plcName, plcDataPoint, plcLocation, plcIp
-    print "post data to Spark"
-    print room_id
-    print spark_token
-    headers = {'content-type': 'application/json', 'Authorization': spark_token}
-    #data = "test from flask server"
-    data = "**Switch:** " + switchName + "\n- **PLC:** " + plcName + "\n- **PLC Data Point:** " + plcDataPoint + "\n- **PLC Location:** " + plcLocation + "\n- **PLC IP Address:** " + plcIp + "\n- **Message:** " + message
-    payload_json1 = {
-        "roomId": room_id,
-        "markdown": data
-    }
-    print payload_json1
-    requests.post(room_url, headers=headers, data=json.dumps(payload_json1))
-    # conv_response_json = spark_post.json()
-    # print conv_response_json
-
-
+# Function to enter the PLC data received from the IE4000 POST to the SQL database
 def plc_data():
     global post_data, switchName, message, plcName, plcDataPoint, plcLocation, plcIp
     conn = sqlite3.connect(db_loc)
@@ -143,7 +110,6 @@ def plc_data():
     print info['plcInfo']['plcDataPoint']
     print info['plcInfo']['plcLocation']
     print info['plcInfo']['plcIp']
-
     switchName = info['switchName']
     message = info['message']
     plcName = info['plcInfo']['plcName']
@@ -156,6 +122,33 @@ def plc_data():
         VALUES (?, ?, ?, ?, ?, ? )''', (switchName, message, plcName, plcDataPoint, plcLocation, plcIp))
     conn.commit()
     print "wrote data to DB"
+
+# Function to get the SIP URI of the Spark Room (future use case)
+@app.route("/uri", methods=['GET'])
+def get_sip_uri():
+    global room_id, spark_token, sip_addr
+    get_room_url = 'https://api.ciscospark.com/v1/rooms/' + str(room_id)
+    headers = {'content-type':'application/json','Authorization': spark_token}
+    payload = {'showSipAddress': 'true'}
+    spark_post = requests.get(get_room_url, headers=headers, params=payload)
+    conv_response_json = spark_post.json()
+    sip_addr = conv_response_json[u'sipAddress']
+    return sip_addr
+
+# Function to post the latest received PLC data to Spark from the SQL database
+def post_spark():
+    global room_url, switchName, message, plcName, plcDataPoint, plcLocation, plcIp
+    print "post data to Spark"
+    print room_id
+    print spark_token
+    headers = {'content-type': 'application/json', 'Authorization': spark_token}
+    data = "**Switch:** " + switchName + "\n- **PLC:** " + plcName + "\n- **PLC Data Point:** " + plcDataPoint + "\n- **PLC Location:** " + plcLocation + "\n- **PLC IP Address:** " + plcIp + "\n- **Message:** " + message
+    payload_json1 = {
+        "roomId": room_id,
+        "markdown": data
+    }
+    print payload_json1
+    requests.post(room_url, headers=headers, data=json.dumps(payload_json1))
 
 
 if __name__ == '__main__':
