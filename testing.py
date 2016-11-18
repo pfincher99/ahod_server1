815a7193049db374d588a97f79b3e5cb69fdb024
Pete Fincher <pfincher@cisco.com>
2016-11-18 15:23:15 -0500

    added test to confirm spark room exists

--- testing.py
+++ testing.py
@@ -26,10 +26,10 @@ class FlaskTestCase(unittest.TestCase):
     # Test to confirm Spark Room Exists
     def test_spark(self):
         print('In test_spark()')
-        room_id = os.getenv("SPARK_ROOM")
+        room_id = os.getenv("spark_room")
         room_url = 'https://api.ciscospark.com/v1/rooms/'+str(room_id)
         print ('URL '+str(room_url))
-        token = os.getenv("SPARK_TOKEN")
+        token = os.getenv("spark_token")
         spark_token = "Bearer " + str(token)
         print ('Spark Token '+str(spark_token))
         headers = {'content-type': 'application/json', 'Authorization': spark_token}
